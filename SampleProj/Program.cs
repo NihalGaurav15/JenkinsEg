d5340d2d997fbe1334bcf3bfb578e23a4c604593
unknown <nihal.gaurav@lntinfotech.com>
2020-09-10 15:27:12 +0530

    Jenkins

--- SampleProj/Program.cs
+++ SampleProj/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,28 @@ namespace SampleProj
 {
     class Program
     {
+        int iNum1 = 0, iNum2 = 0;
+        void GetNumbers()
+        {
+            while(!Int32.TryParse(Console.ReadLine(),out iNum1) || iNum1<=0)
+            {
+                Console.WriteLine("Invalid entry for Num1,Please try again");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out iNum2) || iNum2 <= 0)
+            {
+                Console.WriteLine("Invalid entry for Num2,Please try again");
+            }
+            Add();
+        }
+        private void Add()
+        {
+            int add = iNum1 + iNum2;
+            Console.WriteLine("Sum is"+add);
+        }
         static void Main(string[] args)
         {
+            new Program().GetNumbers();
+            Console.Read();
         }
     }
 }
